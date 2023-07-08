382476e40f8bea643b523860b77f22001843df09
fergussmyth <fergussmyth05@gmail.com>
2023-07-08 13:53:10 +0100

    reformat

--- src/headline_scraper.py
+++ src/headline_scraper.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.bbc.co.uk/news'
-response = requests.get(url)
-
 
 def get_headlines():
+
+    url = 'https://www.bbc.co.uk/news'
+    response = requests.get(url)
+
     soup = BeautifulSoup(response.text, 'html.parser')
     headlines = soup.find('body').find_all('h3')
 
